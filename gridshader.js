378a85a40d942387b287674d109933ec1acde6f0
chrismcginlay <ctmcginlay@gmail.com>
2015-12-20 18:43:55 +0000

    Add an assert function and use it

--- gridshader.js
+++ gridshader.js
@@ -1,13 +1,23 @@
-alert("Javascript loaded");
-
 var blockDimension = 20;    //square blocks size dimension
 var gridSize = 25;          //number of blocks across and down in grid
 var gridPixels = 1;         //pixel width of gridlines
 // pixelWidth - linear number of pixels required to render grid
 var pixelWidth = blockDimension*gridSize+gridPixels*(gridSize+1);
 
+function assert(condition, message) {
+    if (!condition) {
+        message = message || "Assertion Failure";
+        if (typeof Error !== "undefined") {
+            throw new Error(message);
+        }
+        throw message;
+    }
+}
+
 function drawGrid() {
     canvas = document.getElementById('layer-bg-grid');
+    assert(canvas.width==pixelWidth,"Canvas has wrong width");
+    assert(canvas.height==pixelWidth, "Canvas has wrong height");
     if (canvas.getContext) {
         ctx = canvas.getContext('2d');
 
