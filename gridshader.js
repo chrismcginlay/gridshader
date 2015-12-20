6bc62efcc2963fab6953415f4616e5fd57497cea
chrismcginlay <ctmcginlay@gmail.com>
2015-12-20 18:40:30 +0000

    Get the grid drawn

--- gridshader.js
+++ gridshader.js
@@ -1,1 +1,25 @@
 alert("Javascript loaded");
+
+var blockDimension = 20;    //square blocks size dimension
+var gridSize = 25;          //number of blocks across and down in grid
+var gridPixels = 1;         //pixel width of gridlines
+// pixelWidth - linear number of pixels required to render grid
+var pixelWidth = blockDimension*gridSize+gridPixels*(gridSize+1);
+
+function drawGrid() {
+    canvas = document.getElementById('layer-bg-grid');
+    if (canvas.getContext) {
+        ctx = canvas.getContext('2d');
+
+        ctx.beginPath();
+        for (var vline=0; vline<=gridSize; vline++) {
+            ctx.moveTo(0.5+vline*(blockDimension+1), 0);
+            ctx.lineTo(0.5+vline*(blockDimension+1), pixelWidth);
+        }
+        for (var hline=0; hline<=gridSize; hline++) {
+            ctx.moveTo(0, 0.5+hline*(blockDimension+1));
+            ctx.lineTo(pixelWidth, 0.5+hline*(blockDimension+1));
+        }
+        ctx.stroke();
+    }
+}
